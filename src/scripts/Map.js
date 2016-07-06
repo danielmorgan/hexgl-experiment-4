d4a1109cb8139dc2ef7eeac4e1b7154b2757469e
Daniel Morgan <me@danielmorgan.co.uk>
2016-07-06 23:51:23 +0100

    Lock zoom to bounds (glitch on top bound)

--- src/scripts/Map.js
+++ src/scripts/Map.js
@@ -26,10 +26,23 @@ export default class Map extends PIXI.Container {
         this.addChild(this.background);
         this.addChild(this.world);
 
-        // Default zoom and position
+        // Bounds
+        this.bounds = function() {
+            return {
+                left: -(game.layout.size.width * this.scale.x),
+                top: -(game.layout.size.height * this.scale.y),
+                right: window.innerWidth - (this.horizWidth * this.scale.x),
+                bottom: window.innerHeight - (this.vertHeight * this.scale.y)
+            };
+        };
+        console.log(this.bounds());
+
+        // Default zoom
         this.scale.x = this.scale.y = this.minScale();
-        this.x = 0 - (game.layout.size.width * this.scale.x);
-        this.y = 0 - (game.layout.size.height * this.scale.y);
+
+        // Default position
+        this.x = this.bounds().left;
+        this.y = this.bounds().top;
 
         this.bindEvents();
     }
@@ -79,30 +92,60 @@ export default class Map extends PIXI.Container {
         // In or out?
         if (e.deltaY < 0) factor = 1 / factor;
 
-        // Clamp between 20% and 100% zoom
         let newScale = this.scale.x * factor;
-        let minScale = this.minScale() + 0.05;
-        if (newScale > 1 || newScale < minScale) return;
-
-        // Zoom
-        this.scale = new PIXI.Point(newScale, newScale);
+        let minScale = this.minScale();
 
         // Offset so we zoom on mouse cursor
         let dx = -((e.x - this.x) * (factor - 1));
         let dy = -((e.y - this.y) * (factor - 1));
         let delta = new PIXI.Point(dx, dy);
+
+        // Clamp zoom in
+        if (newScale > 1) {
+            this.scale = new PIXI.Point(1, 1);
+        }
+        // Clamp zoom out
+        else if (newScale < minScale) {
+            this.scale = new PIXI.Point(minScale, minScale);
+        }
+        // Zoom
+        else {
+            this.scale = new PIXI.Point(newScale, newScale);
+        }
+
         this.pan(delta);
     }
 
     pan(delta) {
-        let left = -game.layout.size.width;
-        let top = -game.layout.size.height;
-        let right = window.innerWidth - this.width + game.layout.size.width / 2;
-        let bottom = window.innerHeight - this.height + game.layout.size.height / 2;
         let targetX = this.x + delta.x;
         let targetY = this.y + delta.y;
 
-        if (targetX <= left && targetX >= right) this.x = targetX;
-        if (targetY <= top && targetY >= bottom) this.y = targetY;
+        let moved = false;
+
+        // X-axis
+        if (targetX <= this.bounds().left && targetX >= this.bounds().right) {
+            this.x = targetX;
+            moved = true;
+        } else {
+            // Correction
+            if (targetX > this.bounds().left) this.x = this.bounds().left;
+            if (targetX < this.bounds().right) this.x = this.bounds().right;
+        }
+
+        // Y-axis
+        if (targetY <= this.bounds().top && targetY >= this.bounds().bottom) {
+            this.y = targetY;
+            moved = true;
+        } else {
+            // Correction
+            if (targetY > this.bounds().top) this.y = this.bounds().top;
+            if (targetY < this.bounds().bottom) this.y = this.bounds().bottom;
+        }
+
+        if (moved) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
