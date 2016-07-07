164c53268b48f7e2f154a4247045ba93a4f9a056
Daniel Morgan <me@danielmorgan.co.uk>
2016-07-07 22:57:45 +0100

    Tidy up pan constraint logic

--- src/scripts/Map.js
+++ src/scripts/Map.js
@@ -27,7 +27,7 @@ export default class Map extends PIXI.Container {
         this.addChild(this.world);
 
         // Bounds
-        this.bounds = function() {
+        this.bounds = () => {
             return {
                 left: -(game.layout.size.width * this.scale.x),
                 top: -(game.layout.size.height * this.scale.y),
@@ -35,10 +35,10 @@ export default class Map extends PIXI.Container {
                 bottom: window.innerHeight - (this.vertHeight * this.scale.y)
             };
         };
-        console.log(this.bounds());
 
         // Default zoom
         this.scale.x = this.scale.y = this.minScale();
+        console.log(this.bounds());
 
         // Default position
         this.x = this.bounds().left;
@@ -103,17 +103,18 @@ export default class Map extends PIXI.Container {
         // Clamp zoom in
         if (newScale > 1) {
             this.scale = new PIXI.Point(1, 1);
+            this.constrainCamera();
         }
         // Clamp zoom out
         else if (newScale < minScale) {
             this.scale = new PIXI.Point(minScale, minScale);
+            this.constrainCamera();
         }
         // Zoom
         else {
             this.scale = new PIXI.Point(newScale, newScale);
+            this.pan(delta);
         }
-
-        this.pan(delta);
     }
 
     pan(delta) {
@@ -126,26 +127,26 @@ export default class Map extends PIXI.Container {
         if (targetX <= this.bounds().left && targetX >= this.bounds().right) {
             this.x = targetX;
             moved = true;
-        } else {
-            // Correction
-            if (targetX > this.bounds().left) this.x = this.bounds().left;
-            if (targetX < this.bounds().right) this.x = this.bounds().right;
         }
 
         // Y-axis
         if (targetY <= this.bounds().top && targetY >= this.bounds().bottom) {
             this.y = targetY;
             moved = true;
-        } else {
-            // Correction
-            if (targetY > this.bounds().top) this.y = this.bounds().top;
-            if (targetY < this.bounds().bottom) this.y = this.bounds().bottom;
         }
 
-        if (moved) {
-            return true;
-        } else {
-            return false;
+        if (! moved) {
+            this.constrainCamera();
+            return false
         }
+
+        return true;
+    }
+
+    constrainCamera() {
+        if (this.x > this.bounds().left) this.x = this.bounds().left;
+        if (this.x < this.bounds().right) this.x = this.bounds().right;
+        if (this.y > this.bounds().top) this.y = this.bounds().top;
+        if (this.y < this.bounds().bottom) this.y = this.bounds().bottom;
     }
 }
